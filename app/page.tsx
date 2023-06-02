fec2de79b2d057ed785439ddb269a7e4f406f357
Junior Garcia <jrgarciadev@gmail.com>
2023-06-02 11:35:12 -0300

    fix: removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import NextLink from "next/link";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
-import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
