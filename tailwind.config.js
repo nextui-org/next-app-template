00b155026697e2f4adc8eae3034fc75a390b3bcd
WK Wong <wingkwong.code@gmail.com>
2024-07-04 17:29:38 +0800

    chore: sync with page template

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,7 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
