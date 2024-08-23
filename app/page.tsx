e64d0a1a5f86ec8f8e94724fc850245770f830aa
WK Wong <wingkwong.code@gmail.com>
2024-08-23 20:41:24 +0800

    chore(app): change to max-w-xl

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+      <div className="inline-block max-w-xl text-center justify-center">
         <h1 className={title()}>Make&nbsp;</h1>
         <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
         <br />
