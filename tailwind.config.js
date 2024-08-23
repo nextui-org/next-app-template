551de4ed9668693723e15f9512ca0f9e7726efd1
WK Wong <wingkwong.code@gmail.com>
2024-08-23 20:41:38 +0800

    fix(root): add missing fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
