b26bb7f1e01f110e2a50f9d903036bf3e2d54263
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-09-18 10:53:32 +0900

    chore: improve semantics

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <span className={title()}>Make&nbsp;</span>
+        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
-        <h1 className={title()}>
+        <span className={title()}>
           websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
           Beautiful, fast and modern React UI library.
-        </h2>
+        </div>
       </div>
 
       <div className="flex gap-3">
