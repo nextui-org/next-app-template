84ea8690e0df182d371fed468156d4520cdc9c7f
WK Wong <wingkwong.code@gmail.com>
2024-11-19 19:49:03 +0800

    chore(app): remove ThemeProviderProps

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,19 +4,17 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
   children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps) {
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      <NextThemesProvider>{children}</NextThemesProvider>
     </NextUIProvider>
   );
 }
