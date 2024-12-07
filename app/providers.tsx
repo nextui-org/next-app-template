ee0f50754fd242d299558629e010e4fce8c56e02
Junior Garcia <jrgarciadev@gmail.com>
2024-12-07 10:33:13 -0300

    feat: nextui & next.js migration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import type { ThemeProviderProps } from "next-themes";
+
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
+}
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
